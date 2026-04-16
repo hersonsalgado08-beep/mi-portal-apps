05109ef15b937f7bddbbd9e8dba14f5636150440
hersonsalgado08-beep <hersonsalgado08@gmail.com>
2026-04-16 16:33:24 -0700

    Delete app directory
    lj´lj

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>🚀 Portal de Herson: ¡Misión Cumplida!</h1>
-      <p>Si ves esto, tu portal ya está vivo en internet.</p>
-    </div>
-  )
-}
